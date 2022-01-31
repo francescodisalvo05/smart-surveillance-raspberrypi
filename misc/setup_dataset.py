7578fcbe6b9cbb68b719888daa3da614faffecb9
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-01-31 11:54:07 +0100

    Add setup_dataset

--- misc/setup_dataset.py
+++ misc/setup_dataset.py
@@ -8,9 +8,12 @@ Areas & Classes:
 * workshop : duct tape, hammer, sawing
 '''
 
-BASE_PATH = '../assets/dataset_split/'
-DEV = 'dev.csv'
-EVAL = 'eval.csv'
+import csv
+
+
+BASE_PATH = 'assets/dataset_split/'
+DATA_FILES = ['dev.csv', 'eval.csv']
+OUTPUT_FOLDER = 'assets/'
 
 unique_classes = [
     'Speech','Alarm','Drawer_open_or_close','Door','Crying_and_sobbing',
@@ -18,3 +21,43 @@ unique_classes = [
     'Microwave_oven', 'Printer', 'Scissors', 'Computer_keyboard', 'Doorbell', 'Keys_jangling'
     'Knock', 'Ringtone', 'Packing_tape_and_duct_tape', 'Hammer', 'Sawing'
 ]
+
+##############################################################################################
+
+labels = []
+filenames = []
+
+for idx_file in DATA_FILES:
+
+    with open(BASE_PATH + idx_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        # skip header
+        counter = 0
+        for row in csv_reader:
+            if counter == 0:
+                counter += 1
+                continue
+
+            fname = row[0]
+            curr_labels = row[1]
+
+            # there may be some overlapped classes
+            for l in curr_labels.split(","):
+                if l in unique_classes:
+                    labels.append(l)
+                    filenames.append(fname)
+
+
+    output_file = open(OUTPUT_FOLDER + 'idx_' + idx_file, 'w')
+    for label, filename in zip(labels, filenames):
+        output_file.write("{},{}\n".format(filename,label))
+
+    output_file.close()
+
+
+
+
+
+
+
