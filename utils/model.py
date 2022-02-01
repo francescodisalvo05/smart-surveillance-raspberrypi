378e45525c351bdf0cca7e6f161d6139ea736bea
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-02-01 16:28:50 +0100

    Add model setup

---
+++ utils/model.py
@@ -0,0 +1,133 @@
+import os
+import zlib
+import numpy as np
+
+from scipy import signal
+from utils.signal_generator import SignalGenerator
+
+import tensorflow as tf
+import tensorflow_model_optimization as tfmot
+from tensorflow import keras
+
+
+class Model():
+
+    def __init__(self, model_name=None, alpha=1, n_classes=None):
+
+        self.model_name = model_name
+        self.n_classes = n_classes
+
+        self.model = self.set_model(model_name)
+
+        # initialize
+        self.model = None
+        self.tflite_path = None
+        self.model_path = None
+
+
+    def set_model(self,alpha=1): # units = num classes
+        
+        strides=[2,1]
+        units = self.n_classes
+
+        if self.model_name == "DS-CNN":
+
+            self.model = keras.Sequential([
+                  keras.layers.Conv2D(filters=int(alpha*256), kernel_size=[3, 3], strides=strides, use_bias=False),
+                  keras.layers.BatchNormalization(momentum=0.1),
+                  keras.layers.ReLU(),
+
+                  keras.layers.DepthwiseConv2D(kernel_size=[3, 3], strides=[1, 1], use_bias=False),
+                  keras.layers.Conv2D(filters=int(alpha*256), kernel_size=[1, 1], strides=[1, 1], use_bias=False),
+                  keras.layers.BatchNormalization(momentum=0.1),
+                  keras.layers.ReLU(),
+
+                  keras.layers.DepthwiseConv2D(kernel_size=[3, 3], strides=[1, 1], use_bias=False),
+                  keras.layers.Conv2D(filters=int(alpha*256), kernel_size=[1, 1], strides=[1, 1], use_bias=False),
+                  keras.layers.BatchNormalization(momentum=0.1),
+                  keras.layers.ReLU(),
+
+                  keras.layers.GlobalAveragePooling2D(),
+                  keras.layers.Dense(units=units)
+              ])
+
+        else:
+            raise ValueError('{} not implemented'.format(self.model_name))    
+
+
+    def train_model(self, train_ds, val_ds, learning_rate, input_shape, num_epochs, save_model=False):
+            
+        self.model.build(input_shape=input_shape)
+        self.model.compile(optimizer=tf.optimizers.Adam(learning_rate=learning_rate),
+                    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                    metrics=keras.metrics.SparseCategoricalAccuracy())
+
+        self.model.fit(train_ds, epochs=num_epochs, validation_data=val_ds)   
+
+
+    def save_tf(self, path=''):
+
+        self.model_path = path
+
+        if not os.path.isdir(self.model_path):
+            os.mkdir(self.model_path)
+
+        self.model.save(self.model_path)
+
+
+
+    def save_tflite(self, path='', optimization=None):
+        
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
+        self.tflite_path = path
+
+        converter = tf.lite.TFLiteConverter.from_saved_model(path)
+        converter.experimental_enable_resource_variables = True
+
+        if optimization is not None:
+            converter.optimizations = optimization
+
+        tflite_m = converter.convert()
+
+        # save tflite model
+        with open(path, 'wb') as fp:
+            fp.write(tflite_m)
+
+        # compress the tflite model and save it
+        TFLITE_PATH_COMPRESSED = path + ".zlib"
+        with open(TFLITE_PATH_COMPRESSED, 'wb') as fp:
+            compressed_tflite_model = zlib.compress(tflite_m, level=9)
+            fp.write(compressed_tflite_model)
+
+
+    def make_inference(self, test_ds):
+        
+        interpreter = tf.lite.Interpreter(model_path=self.tflite_path)
+        interpreter.allocate_tensors()
+
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+
+        test_ds = test_ds.unbatch().batch(1)
+
+        total_count, correct_count = 0, 0
+
+        for x, y in test_ds:
+            # give the input
+            interpreter.set_tensor(input_details[0]["index"], x)
+            interpreter.invoke()
+
+            # predict and get the current ground truth
+            curr_prediction_logits = interpreter.get_tensor(output_details[0]['index']).squeeze()
+            curr_label = y.numpy().squeeze()
+
+            curr_prediction = np.argmax(curr_prediction_logits)
+
+            if curr_prediction == curr_label:
+                correct_count += 1
+            
+            total_count += 1
+
+        return correct_count / total_count
