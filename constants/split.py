6327ca38e43864477594c2fd37d80bde0afc693a
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-02-02 14:01:21 +0100

    Init dictionaries

---
+++ constants/split.py
@@ -0,0 +1,47 @@
+'''
+Areas & Classes:
+* bedroom : speech, alarm, drawer open or close, door, crying, mechanical fan, ringtone
+* bathroom : speech, sink, toilet flush
+* kitchen : speech, alarm, boiling, sink, water tap, microwave oven
+* office : speech, alarm, printer, scissors, computer keyboard, ringtone
+* entrance : speech, doorbell, keys jangling, knock, ringtone
+* workshop : duct tape, hammer, sawing
+'''
+
+# TO DO : Add the remaining classes
+
+ROOM_DICTIONARY = {
+
+    'ALL' : {
+        'Alarm': 0,
+        'Computer_keyboard': 0,
+        'Crying_and_sobbing': 0,
+        'Door': 0,
+        'Doorbell': 0,
+        'Drawer_open_or_close': 0,
+        'Hammer': 0,
+        'Keys_jangling': 0,
+        'Knock': 0,
+        'Mechanical_fan': 0,
+        'Microwave_oven': 0,
+        'Packing_tape_and_duct_tape': 0,
+        'Printer': 0,
+        'Ringtone': 0,
+        'Sawing': 0,
+        'Scissors': 0,
+        'Sink_(filling_or_washing)': 0,
+        'Speech': 0,
+        'Water_tap_and_faucet': 0
+    },
+
+    'BEDROOM' : {
+        'Alarm': 0,
+        'Crying_and_sobbing': 0,
+        'Door': 0,
+        'Drawer_open_or_close': 0,
+        'Mechanical_fan': 0,
+        'Ringtone': 0,
+        'Speech': 0,
+    }
+
+}
