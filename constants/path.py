51cd4b605d64ccf17bcf6fe0bfddb89d3c7ef696
GianlucaLM-1 <66356627+GianlucaLM-1@users.noreply.github.com>
2022-02-03 12:16:09 +0100

    Update SPLIT_BASE_PATH

--- constants/path.py
+++ constants/path.py
@@ -1,6 +1,7 @@
 DATASET = 'data/Dataset/'
 DATASET_IDX = 'assets/dataset_idx/idx_dev.csv'
+DATASET_EVAL_IDX = 'assets/dataset_idx/eval_ids.csv'
 
 # It is based on Google Colab.. Fix it
-SPLIT_BASE_PATH = '/content/domestic-sounds/assets/dataset_split'
+SPLIT_BASE_PATH = 'assets/dataset_split'
 
