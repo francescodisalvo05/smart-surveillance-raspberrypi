6c839b3857923c55bbc8dcc4c6f22b7216f8dc83
GianlucaLM-1 <66356627+GianlucaLM-1@users.noreply.github.com>
2022-02-03 12:16:45 +0100

    Update create_splits function

--- misc/create_splits.py
+++ misc/create_splits.py
@@ -5,14 +5,23 @@ import argparse, os
 import sys
 sys.path.append('constants/')
 
+
 from split import ROOM_DICTIONARY
-from path import DATASET, SPLIT_BASE_PATH, DATASET_IDX
+
+DATASET = 'data/Dataset/'
+DATASET_IDX = 'assets/dataset_idx/idx_dev.csv'
+DATASET_EVAL_IDX = 'assets/dataset_idx/eval_ids.csv'
+
+# It is based on Google Colab.. Fix it
+SPLIT_BASE_PATH = 'assets/dataset_split'
 
 parser = argparse.ArgumentParser()
 
 # to do: define all the other choices for room
 parser.add_argument('-r','--room',type=str, default='all', choices=['all','bedroom'])
 parser.add_argument('-d','--dataset',type=str, default=DATASET_IDX)
+parser.add_argument('-m','--mode',type=str, choices = ['train','val-test'] ,default = 'train')
+
 args = parser.parse_args()
 
 df = pd.read_csv(args.dataset, names=['fname','label'])
@@ -22,6 +31,8 @@ room_dictionary = ROOM_DICTIONARY[args.room]
 
 TRAIN_NAME = 'train_{}_split.txt'.format(args.room)
 VALIDATION_NAME = 'val_{}_split.txt'.format(args.room)
+TEST_NAME = 'test_{}_split.txt'.format(args.room)
+
 
 # get only 100 records
 # they will be augumented later
@@ -37,19 +48,27 @@ df_new = pd.DataFrame({'fname' : fnames, 'label' : classes})
 y = df_new.label
 X = df_new.drop(columns=['label'])
 
-# split it into train and validation
-x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.25, stratify = y)
+if args.mode == 'val-test':
+  # split it into train and validation
+  x_test, x_val, y_test, y_val = train_test_split(X, y, test_size=0.20, stratify = y)
 
-with open(os.path.join(SPLIT_BASE_PATH,TRAIN_NAME),'w') as file:
+  with open(os.path.join(SPLIT_BASE_PATH,TEST_NAME),'w') as file:
 
-  for idx in range(len(x_train)):
-    string = './{}/{}/{}.wav\n'.format(DATASET,y_train.iloc[idx], x_train.iloc[idx].fname)
-    file.write(string)
+    for idx in range(len(x_test)):
+      string = './{}/{}/{}.wav\n'.format(DATASET,y_test.iloc[idx], x_test.iloc[idx].fname)
+      file.write(string)
 
-with open(os.path.join(SPLIT_BASE_PATH,VALIDATION_NAME),'w') as file:
+  with open(os.path.join(SPLIT_BASE_PATH,VALIDATION_NAME),'w') as file:
 
-  for idx in range(len(x_val)):
-    string = './{}/{}/{}.wav\n'.format(DATASET, y_val.iloc[idx], x_val.iloc[idx].fname)
-    file.write(string)
+    for idx in range(len(x_val)):
+      string = './{}/{}/{}.wav\n'.format(DATASET, y_val.iloc[idx], x_val.iloc[idx].fname)
+      file.write(string)
 
-file.close()
+  file.close()
+
+else:
+  with open(os.path.join(SPLIT_BASE_PATH,TRAIN_NAME),'w') as file:
+
+    for idx in range(len(X)):
+      string = './{}/{}/{}.wav\n'.format(DATASET,y.iloc[idx], X.iloc[idx].fname)
+      file.write(string)
