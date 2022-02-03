631ab6bcc3d10fcc8313192407c6cdae76323d01
GianlucaLM-1 <66356627+GianlucaLM-1@users.noreply.github.com>
2022-02-03 12:17:01 +0100

    Add creation of test_ds

--- utils/data.py
+++ utils/data.py
@@ -15,10 +15,15 @@ def get_data(room, labels, resampling, mfcc_options):
     with open('{}/val_{}_split.txt'.format(SPLIT_BASE_PATH, room) ,"r") as fp:
        val_files = [line.rstrip() for line in fp.readlines()]
 
+    with open('{}/test_{}_split.txt'.format(SPLIT_BASE_PATH, room) ,"r") as fp:
+       test_files = [line.rstrip() for line in fp.readlines()]
+
     generator = SignalGenerator(labels, sampling_rate=44100, resampling_rate=resampling, **mfcc_options)
     train_ds = generator.make_dataset(train_files, True)
     val_ds = generator.make_dataset(val_files, False)
+    test_ds = generator.make_dataset(test_files, False)
+
     
     # test_ds = generator.make_dataset(test_files, False)
 
-    return train_ds, val_ds #, test_ds
+    return train_ds, val_ds , test_ds
