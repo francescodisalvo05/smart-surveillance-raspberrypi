00afee70e14affd1dda413fa0d49e192e2fd5999
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-02-04 10:25:39 +0100

    Unify train and test

--- misc/create_splits.py
+++ misc/create_splits.py
@@ -3,15 +3,15 @@ import pandas as pd
 import argparse, os
 
 import sys
-sys.path.append('constants/')
 
+sys.path.append('constants/')
 
 from split import ROOM_DICTIONARY
 
 DATASET_TRAIN = 'data/Dataset_train/'
 DATASET_TEST = 'data/Dataset_test/'
 
-DATASET_IDX = 'assets/dataset_idx/dev_ids.csv'
+DATASET_DEV_IDX = 'assets/dataset_idx/dev_ids.csv'
 DATASET_EVAL_IDX = 'assets/dataset_idx/eval_ids.csv'
 
 # It is based on Google Colab.. Fix it
@@ -21,12 +21,14 @@ parser = argparse.ArgumentParser()
 
 # to do: define all the other choices for room
 parser.add_argument('-r','--room',type=str, default='all', choices=['all','bedroom','bathroom','kitchen','office','entrance','workshop'])
-parser.add_argument('-d','--dataset',type=str, default=DATASET_IDX)
-parser.add_argument('-m','--mode',type=str, choices = ['train','val-test'] ,default = 'train')
 
 args = parser.parse_args()
 
-df = pd.read_csv(args.dataset, names=['fname','label'])
+##########################################################
+######### TRAIN / VAL
+##########################################################
+
+df = pd.read_csv(DATASET_DEV_IDX, names=['fname', 'label'])
 
 fnames, classes = [], []
 room_dictionary = ROOM_DICTIONARY[args.room]
@@ -35,42 +37,45 @@ TRAIN_NAME = 'train_{}_split.txt'.format(args.room)
 VALIDATION_NAME = 'val_{}_split.txt'.format(args.room)
 TEST_NAME = 'test_{}_split.txt'.format(args.room)
 
-
 # get only 100 records
 # they will be augumented later
-for index,row in df.iterrows():
-    if df.loc[index,'label'] in list(room_dictionary.keys()):
-      if room_dictionary[df.loc[index,'label']] < 100:
-        fnames.append(df.loc[index,'fname'])
-        classes.append(df.loc[index,'label'])
-        room_dictionary[df.loc[index,'label']] += 1
-     
-df_new = pd.DataFrame({'fname' : fnames, 'label' : classes})
+for index, row in df.iterrows():
+    if df.loc[index, 'label'] in list(room_dictionary.keys()):
+        if room_dictionary[df.loc[index, 'label']] < 120:
+            fnames.append(df.loc[index, 'fname'])
+            classes.append(df.loc[index, 'label'])
+            room_dictionary[df.loc[index, 'label']] += 1
+
+df_new = pd.DataFrame({'fname': fnames, 'label': classes})
 
 y = df_new.label
 X = df_new.drop(columns=['label'])
 
-if args.mode == 'val-test':
-  # split it into train and validation
-  x_test, x_val, y_test, y_val = train_test_split(X, y, test_size=0.20, stratify = y)
+# split it into train and validation
+x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.10, stratify=y)
 
-  with open(os.path.join(SPLIT_BASE_PATH,TEST_NAME),'w') as file:
+with open(os.path.join(SPLIT_BASE_PATH, TRAIN_NAME), 'w') as file:
+    for idx in range(len(x_train)):
+        string = './{}{}/{}.wav\n'.format(DATASET_TRAIN, y_train.iloc[idx], x_train.iloc[idx].fname)
+        file.write(string)
 
-    for idx in range(len(x_test)):
-      string = './{}/{}/{}.wav\n'.format(DATASET_TEST,y_test.iloc[idx], x_test.iloc[idx].fname)
-      file.write(string)
-
-  with open(os.path.join(SPLIT_BASE_PATH,VALIDATION_NAME),'w') as file:
-
+with open(os.path.join(SPLIT_BASE_PATH, VALIDATION_NAME), 'w') as file:
     for idx in range(len(x_val)):
-      string = './{}/{}/{}.wav\n'.format(DATASET_TEST, y_val.iloc[idx], x_val.iloc[idx].fname)
-      file.write(string)
+        string = './{}{}/{}.wav\n'.format(DATASET_TEST, y_val.iloc[idx], x_val.iloc[idx].fname)
+        file.write(string)
 
-  file.close()
+file.close()
 
-else:
-  with open(os.path.join(SPLIT_BASE_PATH,TRAIN_NAME),'w') as file:
+##########################################################
+######### TEST
+##########################################################
 
+df = pd.read_csv(DATASET_EVAL_IDX, names=['fname', 'label'])
+
+y = df_new.label
+X = df_new.drop(columns=['label'])
+
+with open(os.path.join(SPLIT_BASE_PATH, TEST_NAME), 'w') as file:
     for idx in range(len(X)):
-      string = './{}/{}/{}.wav\n'.format(DATASET_TRAIN,y.iloc[idx], X.iloc[idx].fname)
-      file.write(string)
+        string = './{}{}/{}.wav\n'.format(DATASET_TRAIN, y.iloc[idx], X.iloc[idx].fname)
+        file.write(string)
