27b07c8151d0c24b9e1f4f53626e5147abb4558f
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-02-15 16:44:10 +0100

    Add AUGUMENTATION_PATH

--- constants/path.py
+++ constants/path.py
@@ -3,4 +3,5 @@ DATASET_IDX = 'assets/dataset_idx/idx_dev.csv'
 
 # It is based on Google Colab.. Fix it
 SPLIT_BASE_PATH = '/content/domestic-sounds/assets/dataset_split'
+AUGUMENTATION_PATH = '/content/domestic-sounds/data/Dataset_Augumented/'
 
