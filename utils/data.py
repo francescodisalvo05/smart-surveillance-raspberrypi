250a4f3afdea2c8167f5a670318472add9d81464
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-02-23 09:24:30 +0100

    Remove room

--- utils/data.py
+++ utils/data.py
@@ -9,13 +9,13 @@ from utils.signal_generator import SignalGenerator
 
 def get_data(room, labels, resampling, mfcc_options):
 
-    with open('{}/train_{}_split.txt'.format(SPLIT_BASE_PATH, room) ,"r") as fp:
+    with open('{}/train_split.txt'.format(SPLIT_BASE_PATH) ,"r") as fp:
        train_files = [line.rstrip() for line in fp.readlines()]
     
-    with open('{}/val_{}_split.txt'.format(SPLIT_BASE_PATH, room) ,"r") as fp:
+    with open('{}/val_split.txt'.format(SPLIT_BASE_PATH) ,"r") as fp:
        val_files = [line.rstrip() for line in fp.readlines()]
 
-    with open('{}/test_{}_split.txt'.format(SPLIT_BASE_PATH, room) ,"r") as fp:
+    with open('{}/test_split.txt'.format(SPLIT_BASE_PATH) ,"r") as fp:
        test_files = [line.rstrip() for line in fp.readlines()]
 
     generator = SignalGenerator(labels, sampling_rate=44100, resampling_rate=resampling, **mfcc_options)
