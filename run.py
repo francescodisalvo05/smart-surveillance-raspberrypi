ca2c0e114afdb3e206b6d9443269a9e9789ffccc
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-02-23 20:27:21 +0100

    Fix audio recording

--- run.py
+++ run.py
@@ -10,9 +10,8 @@ from datetime import datetime
 from scipy.io import wavfile
 from argparse import ArgumentParser
 
-
-
-
+import logging
+logging.getLogger().setLevel(logging.INFO)
 
 from MQTT.DoSomething import DoSomething
 
@@ -23,23 +22,25 @@ def main(args):
 
     publisher = DoSomething("Publisher")
     publisher.run()
-    
+
     while True:
         # record 4s (temporary)
         # to do: audio preprocessing
-        tf_audio = record_audio(args, p)
+        tf_audio = record_audio(args, p)   
         
         # to do: convert number to label
-        prediction, probability = make_inference(tf_audio, args.tflite_path)
+        # prediction, probability = make_inference(tf_audio, args.tflite_path)
 
         # publish via MQTT
-        publish_outcome(publisher, prediction, probability, args.room)
+        # publish_outcome(publisher, prediction, probability, args.room)
         
         
 
 def record_audio(args, p):
 
-    stream = p.open(format=format, channels=args.channels, rate=args.rate, input=True, frames_per_buffer=args.chunk)
+    logging.info('Start recoding...')
+
+    stream = p.open(format=pyaudio.paInt16, channels=args.channels, rate=args.rate, input=True, frames_per_buffer=args.chunk)
 
     frames = []
     for _ in range(0,int(args.rate / args.chunk * args.seconds)):
@@ -49,17 +50,18 @@ def record_audio(args, p):
     stream.stop_stream()
     stream.close()
 
-    container = io.BytesIO()
-    wf = wave.open(container, 'wb')
+    wf = wave.open('filename.wav', 'wb')
     wf.setnchannels(args.channels)
-    wf.setsampwidth(p.get_sample_size(format))
+    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
     wf.setframerate(args.rate)
     wf.writeframes(b''.join(frames))    
+    wf.close() 
     
-    tf_audio, _ = tf.audio.decode_wav(container)
-    tf_audio = tf.squeeze(tf_audio, 1)
+    audio = tf.io.read_file('filename.wav')
+    tf_audio, _ = tf.audio.decode_wav(audio) 
 
-    wf.close()   
+    logging.info('End recoding...')  
+
 
     return tf_audio
 
@@ -109,7 +111,7 @@ if __name__ == '__main__':
     parser.add_argument('--chunk', type=int, default=1024, help='Set number of chunks')
     parser.add_argument('--format', type=str, default='Int16', help='Set the format of the audio track [Int8,Int16,Int32]')
     parser.add_argument('--channels', type=int, default=2, help='Set the number of channels')
-    parser.add_argument('--seconds', type=int, default=4, help='Set the length of the recording (seconds)')
+    parser.add_argument('--seconds', type=int, default=10, help='Set the length of the recording (seconds)')
     parser.add_argument('--rate', type=int, default=44100, help='Set the rate')
     parser.add_argument('--name', type=str, default=None, help='Set the name of the audio track')
 
