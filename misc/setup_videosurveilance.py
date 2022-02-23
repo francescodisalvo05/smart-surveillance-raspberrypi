d7c4b30ca56f9ef13ad8176601a0ac4c306a10e6
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-02-23 09:00:36 +0100

    Update classes

--- misc/setup_videosurveilance.py
+++ misc/setup_videosurveilance.py
@@ -5,7 +5,7 @@ DATA_FILES = ['dev.csv', 'eval.csv']
 OUTPUT_FOLDER = 'assets/dataset_idx/'
 
 unique_fixed_classes = [
-    'Bark', 'Knock', 'Drill', 'Hammer',
+    'Bark', 'Door', 'Drill', 'Hammer',
     'Fire', 'Gunshot_and_gunfire'
 ]
 
@@ -30,30 +30,29 @@ for idx_file in DATA_FILES:
 
             fname = row[0]
             curr_labels = row[1]
-            found = False
 
             # there may be some overlapped classes
             current_labels = curr_labels.split(",")
             for l in current_labels:
                 if l in unique_fixed_classes:
-                    found = True
+
                     labels.append(l)
                     filenames.append(fname)
             
-            if not found:
-                # particular cases
-                if 'Slam' in current_labels and \
-                'Door' in current_labels and \
-                'Motor_vehicle_(road)' not in current_labels:
-                    labels.append('Slam')
-                    filenames.append(fname)
-                
-                # (Glass && (Shatter or Explosion)) or Crushing'''
-                elif ('Glass' in current_labels and \
-                        ('Shatter' in current_labels or 'Explosion' in current_labels)) or \
-                    'Crushing' in current_labels:
-                    labels.append('Glass')
-                    filenames.append(fname)
+
+            # particular cases
+            if ('Slam' in current_labels) and \
+                ('Door' in current_labels) and \
+                not ('Motor_vehicle_(road)' in current_labels):
+                labels.append('Slam')
+                filenames.append(fname)
+            
+            # (Glass && (Shatter or Explosion)) or Crushing'''
+            elif ('Glass' in current_labels and \
+                    ('Shatter' in current_labels or 'Explosion' in current_labels)) or \
+                'Crushing' in current_labels:
+                labels.append('Glass')
+                filenames.append(fname)
 
 
     output_file = open(OUTPUT_FOLDER + 'new_idx_' + idx_file, 'w')
