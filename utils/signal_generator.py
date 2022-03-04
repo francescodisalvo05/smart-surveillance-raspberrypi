c9a359b4d2663620e18c9841217cb2e999ccd11c
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-03-04 10:09:09 +0100

    Remove room

--- utils/signal_generator.py
+++ utils/signal_generator.py
@@ -129,14 +129,11 @@ class SignalGenerator:
 
         return mfccs, label
 
-    def make_dataset(self, files, train, augumentation_path, room=None):
+    def make_dataset(self, files, train, augumentation_path):
 
         
         # duplicate audios for augumentation
         if augumentation_path:
-
-            if not room:
-              raise ValueError("You must specify the room!")
 
             labels_to_augument = [l for l in ROOM_DICTIONARY[room].keys() if l not in ['Speech','Alarm']]
 
