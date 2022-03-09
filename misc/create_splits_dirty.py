80fbbf940e26dba52df886ba5d8d41c2e98cb134
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-03-09 08:22:15 +0100

    Update splits for benchmark

--- misc/create_splits_dirty.py
+++ misc/create_splits_dirty.py
@@ -13,7 +13,17 @@ SPLIT_BASE_PATH = 'assets/dataset_split/'
 
 TRAIN_NAME = 'train_split_dirty.txt'
 VALIDATION_NAME = 'val_split_dirty.txt'
-TEST_NAME = 'test_split_dirty.txt'
+TEST_NAME = 'test_split.txt'
+
+# read test file from the cleaned one
+x_clean = []
+with open(os.path.join(SPLIT_BASE_PATH, TEST_NAME), 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        x_clean.append(line.strip().split("/")[-1])
+file.close()
+
+print(x_clean)
 
 fnames, classes = [], []
 
@@ -27,24 +37,18 @@ df = pd.DataFrame({'fname':fnames, 'label':classes})
 y = df.label
 X = df.drop(columns=['label'])
 
-
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=RANDOM_STATE)
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.13, stratify=y_train, random_state=RANDOM_STATE) 
+x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.13, stratify=y, random_state=RANDOM_STATE) 
 
 with open(os.path.join(SPLIT_BASE_PATH, TRAIN_NAME), 'w') as file:
     for idx in range(len(x_train)):
-        string = './{}{}/{}\n'.format(DATASET, y_train.iloc[idx], x_train.iloc[idx].fname)
-        file.write(string)
+        if x_train.iloc[idx].fname not in x_clean:
+            string = './{}{}/{}\n'.format(DATASET, y_train.iloc[idx], x_train.iloc[idx].fname)
+            file.write(string)
 file.close()
 
 with open(os.path.join(SPLIT_BASE_PATH, VALIDATION_NAME), 'w') as file:
     for idx in range(len(x_val)):
-        string = './{}{}/{}\n'.format(DATASET, y_val.iloc[idx], x_val.iloc[idx].fname)
-        file.write(string)
-file.close()
-
-with open(os.path.join(SPLIT_BASE_PATH, TEST_NAME), 'w') as file:
-    for idx in range(len(x_val)):
-        string = './{}{}/{}\n'.format(DATASET, y_test.iloc[idx], x_test.iloc[idx].fname)
-        file.write(string)
+        if x_train.iloc[idx].fname not in x_clean:
+            string = './{}{}/{}\n'.format(DATASET, y_val.iloc[idx], x_val.iloc[idx].fname)
+            file.write(string)
 file.close()
