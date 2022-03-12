71def95d5476e3203e1047c5d0dd8a89049cc1ee
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-03-12 17:52:24 +0100

    Move constants

--- constants/path.py
+++
@@ -1,7 +0,0 @@
-DATASET = 'data/Dataset/'
-DATASET_IDX = 'assets/dataset_idx/idx_dev.csv'
-
-# It is based on Google Colab.. Fix it
-SPLIT_BASE_PATH = '/content/domestic-sounds/assets/dataset_split'
-AUGUMENTATION_PATH = '/content/domestic-sounds/data/Dataset_Augumented/'
-
