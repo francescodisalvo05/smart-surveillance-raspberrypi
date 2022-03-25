f0a8afc35bfdcdc99290ed2cd7ea32a3a23535d0
Leonardo Maggio <48280683+LeoMaggio@users.noreply.github.com>
2022-03-25 09:24:10 +0100

    Update signal_generator.py

--- src/audio/utils/signal_generator.py
+++ src/audio/utils/signal_generator.py
@@ -7,7 +7,7 @@ from scipy import signal
 class SignalGenerator:
     def __init__(self, labels, sampling_rate, frame_length, frame_step,
                  num_mel_bins=None, lower_frequency=None, upper_frequency=None,
-                 num_coefficients=None, mfcc=False, resampling_rate=None, seconds=2):
+                 num_coefficients=None, mfcc=False, resampling_rate=None, seconds=1):
 
         self.labels = labels
 
