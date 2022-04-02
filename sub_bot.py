eecc36c1bf689861c35c494e55a0af42f849b15e
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-04-02 14:42:54 +0200

    Integration with external triggers

--- sub_bot.py
+++ sub_bot.py
@@ -1,5 +1,5 @@
-from DoSomething import DoSomething
-from bot.botds import Bot
+from MQTT.DoSomething import DoSomething
+
 
 import time
 import json
@@ -9,34 +9,28 @@ from picamera import PiCamera
 
 
 class Subscriber(DoSomething):
-    def __init__(self):
-        self.bot = Bot(True)
-        self.bot.run()
 
     def notify(self, topic, msg):
         input_json = json.loads(msg)
         
         timestamp = input_json['timestamp']
+        # use them?
         label = input_json['class']
         confidence = input_json['confidence']
 
         # send message here
-        # self.bot.send_message(timestamp,label,confidence)
-
-        now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-
         camera = PiCamera()
         camera.start_preview()
         
         time.sleep(2) # focus
-        img_path = './assets/storage/{}.jpg'.format(now)
+        img_path = './assets/storage/{}.jpg'.format('test') # use a clear version of timestamp
         
         camera.capture(img_path)
         camera.stop_preview()
 
-        # start the streaming
+        # start streaming? 
 
-        # self.bot.send_img(img_path)
+        self.bot.send_alarm(timestamp,img_path)
 
 
         
