dfc7915266e423311dbfdb1b842463adc212166a
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-04-02 14:42:32 +0200

    Clean class and handle images

--- bot/botds.py
+++ bot/botds.py
@@ -14,23 +14,6 @@ import numpy as np
 import math
 
 
-TOKEN = '5165021744:AAGqhFc_5heY5EXaBulsRy_HGeC67diZFGs'
-
-
-def send_text(bot_message,chat_id):
-    send_text = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + str(chat_id) + '&parse_mode=Markdown&text=' + bot_message
-    response = requests.get(send_text)
-    return response.json()
-
-def read_db():      
-    df = pd.read_csv("users.csv")
-    return df
-
-def update_db(df,index,column,value):
-    df.at[index, column] = value
-    df.to_csv("users.csv")
-
-
 class Bot:
 
     def __init__(self,danger) -> None:
@@ -38,12 +21,14 @@ class Bot:
             format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
             level=logging.INFO,
         )
+
+        self.TOKEN = '5165021744:AAGqhFc_5heY5EXaBulsRy_HGeC67diZFGs'
+
         self.logger = logging.getLogger("LOG")
         self.logger.info("Starting BOT.")
-        self.updater = Updater(TOKEN)
+        self.updater = Updater(self.TOKEN)
         self.dispatcher = self.updater.dispatcher
         self.danger = danger
-        
         
         
         enable_handler = CommandHandler("enable", self.send_enable)
@@ -85,7 +70,7 @@ class Bot:
     def send_enable(self, chatbot, update) -> None:
         # write the chat id in the database
         chat_id = chatbot.message.chat_id
-        db = read_db()
+        db = self._read_db()
         db.set_index('ids',inplace = True)
 
         if chat_id in db.index:
@@ -94,7 +79,7 @@ class Bot:
                 chatbot.message.reply_text(enable_message)
             else:
                 enable_message = '✅ Riceverai tutti gli aggiornamenti sulla tua casa'
-                update_db(db,chat_id,'status',True)
+                self._update_db(db,chat_id,'status',True)
                 chatbot.message.reply_text(enable_message)
         else:
             enable_message = 'Non sei abilitato a questo servizio, contattare gli amministatori'
@@ -103,14 +88,14 @@ class Bot:
     def send_disable(self, chatbot, update) -> None:
         # write the chat id in the database
         chat_id = chatbot.message.chat_id
-        db = read_db()
+        db = self._read_db()
         db.set_index('ids',inplace = True)
 
         if chat_id in db.index:
             if db.at[chat_id,'status'] == True:
                 enable_message = '❌ Non riceverai più alcuna notifica'
                 chatbot.message.reply_text(enable_message)
-                update_db(db,chat_id,'status',False)
+                self._update_db(db,chat_id,'status',False)
 
             else:
                 enable_message = 'Non sei iscritto al servizio di notifiche'
@@ -129,7 +114,7 @@ class Bot:
     # message to send when /enable is received
     def live_video(self, chatbot, update) -> None:
         chat_id = chatbot.message.chat_id
-        db = read_db()
+        db = self._read_db()
         db.set_index('ids',inplace = True)
         
         if chat_id in db.index:
@@ -149,16 +134,16 @@ class Bot:
     def report(self, chatbot, update) -> None:
         string = ""
         chat_id = chatbot.message.chat_id
-        db = read_db()
+        db = self._read_db()
         db.set_index('ids',inplace = True)
 
         if chat_id in db.index:
-            if not os.path.exists("reports.txt"):
+            if not os.path.exists("bot/reports.txt"):
                 chatbot.message.reply_text("Non ci sono avvertimenti")
             
 
             enable_message = 'Ciao "User" ecco lo storico degli avvertimenti'
-            reports = np.genfromtxt('reports.txt',dtype='str')
+            reports = np.genfromtxt('bot/reports.txt',dtype='str')
             for report in reports:
                 string += "•" + report + "\n"
             chatbot.message.reply_text(enable_message)
@@ -168,34 +153,35 @@ class Bot:
             chatbot.message.reply_text("Accesso negato❌")
 
     # start the bot
-    def run(self) -> int:
+    def send_alarm(self,timestampm,img_path) -> int:
 
         reports = []
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
-        db = read_db()
+        db = self._read_db()
         db.set_index('ids',inplace = True)
-
-        if self.danger:
             
-            if db.at[99706017,"status"] == True:
-                message = "⚠️ *Allarme Intrusione* ⚠️\n" + "🕚 Orario: " + current_time + "\n\nDai un'occhiata a cosa sta succedendo:\nhttp://raspberrypi.local:8000/index.html"
-                send_text(message,99706017)
-            
+        if db.at[99706017,"status"] == True:
+            message = "⚠️ *Allarme Intrusione* ⚠️\n" + "🕚 Orario: " + current_time + "\n\nDai un'occhiata a cosa sta succedendo:\nhttp://raspberrypi.local:8000/index.html"
+            self._send_text(message,99706017)
+            self._send_photo(open(img_path, 'rb'),99706017)
         
-            if db.at[129347830,"status"] == True:
-                message = "⚠️ *Allarme Intrusione* ⚠️\n" + "🕚 Orario: " + current_time + "\n\nDai un'occhiata a cosa sta succedendo: \n\nhttp://raspberrypi.local:8000/index.html"
-                send_text(message,129347830)
-            
+    
+        if db.at[129347830,"status"] == True:
+            message = "⚠️ *Allarme Intrusione* ⚠️\n" + "🕚 Orario: " + current_time + "\n\nDai un'occhiata a cosa sta succedendo: \n\nhttp://raspberrypi.local:8000/index.html"
+            self._send_text(message,129347830)
+            self._send_photo(open(img_path, 'rb'),129347830)
+        
 
-            if db.at[171207972,"status"] == True:
-                message = "⚠️ *Allarme Intrusione* ⚠️\n" + "🕚 Orario: " + current_time + "\n\nDai un'occhiata a cosa sta succedendo: \n\nhttp://raspberrypi.local:8000/index.html"
-                send_text(message,171207972)
-            
-            reports.append(current_time)
-            arr = np.genfromtxt('reports.txt',dtype='str')
-            reports = np.append(arr,str(current_time))
-            np.savetxt('reports.txt', reports, delimiter=" ", fmt="%s")
+        if db.at[171207972,"status"] == True:
+            message = "⚠️ *Allarme Intrusione* ⚠️\n" + "🕚 Orario: " + current_time + "\n\nDai un'occhiata a cosa sta succedendo: \n\nhttp://raspberrypi.local:8000/index.html"
+            self._send_text(message,171207972)
+            self._send_photo(open(img_path, 'rb'),171207972)
+        
+        reports.append(current_time)
+        arr = np.genfromtxt('bot/reports.txt',dtype='str')
+        reports = np.append(arr,str(current_time))
+        np.savetxt('reports.txt', reports, delimiter=" ", fmt="%s")
     
         self.logger.info("Polling BOT.")
         self.updater.start_polling()
@@ -204,3 +190,24 @@ class Bot:
         # start_polling() is non-blocking and will stop the BOT gracefully.
         self.updater.idle()
 
+
+    def _send_text(self,bot_message,chat_id):
+        send_text = 'https://api.telegram.org/bot' + self.TOKEN + '/sendMessage?chat_id=' + str(chat_id) + '&parse_mode=Markdown&text=' + bot_message
+        response = requests.get(send_text)
+        return response.json()
+
+    def _send_photo(self,file_opened,bot_chatID):
+        method = "sendPhoto"
+        params = {'chat_id': bot_chatID}
+        files = {'photo': file_opened}
+        url = 'https://api.telegram.org/bot' + self.TOKEN + "/"
+        response = requests.post(url + method, params, files=files)
+        return response.json()
+
+    def _read_db(self):      
+        df = pd.read_csv("bot/users.csv")
+        return df
+
+    def _update_db(self,df,index,column,value):
+        df.at[index, column] = value
+        df.to_csv("bot/users.csv")
