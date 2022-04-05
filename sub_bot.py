c56b8c7a23513839844759be973e3e517907253c
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-04-05 17:51:20 +0200

    Add video recording

--- sub_bot.py
+++ sub_bot.py
@@ -1,12 +1,14 @@
 from MQTT.DoSomething import DoSomething
 
-
+import os
 import time
 import json
 import datetime
 
 from picamera import PiCamera
 from bot.botds import Bot
+
+import subprocess
 
 
 class Subscriber(DoSomething):
@@ -19,20 +21,35 @@ class Subscriber(DoSomething):
         label = input_json['class']
         confidence = input_json['confidence']
 
-        img_path = './assets/storage/{}.jpg'.format('test')
+        time_str = time.strftime("%Y%m%d-%H%M%S")
+        video_path = './assets/storage/{}.h264'.format(time_str)
+        new_video_path = './assets/storage/{}.mp4'.format(time_str)
 
+        
+
+        """
+        img_path = './assets/storage/img.jpeg'
         camera = PiCamera()
         camera.start_preview()    
         camera.capture(img_path)
         camera.stop_preview()
+        camera.close()"""
+
+        camera = PiCamera()
+        camera.resolution = (480, 360)
+        camera.rotation = 180
+        camera.start_recording(video_path)
+        camera.wait_recording(30)
+        camera.stop_recording()
         camera.close()
 
-        # start streaming? 
+        command = "ffmpeg -i {} -y -vf scale=-1:360 {}".format(video_path, new_video_path)
+        subprocess.call([command], shell=True)
 
-        self.bot.send_alarm(timestamp,img_path)
+        self.bot.send_alarm(timestamp,new_video_path)
 
+        os.remove(video_path)
 
-        
 
 if __name__ == "__main__":
     test = Subscriber("Bot")
