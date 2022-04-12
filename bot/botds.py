7f2131ce931695483c2e1f9820e3960f1c335155
GianlucaLM-1 <66356627+GianlucaLM-1@users.noreply.github.com>
2022-04-12 17:50:54 +0200

    Update telegram bot

--- bot/botds.py
+++ bot/botds.py
@@ -17,7 +17,7 @@ from bot_settings import *
 
 class Bot:
 
-    def __init__(self,danger) -> None:
+    def __init__(self) -> None:
         logging.basicConfig(
             format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
             level=logging.INFO,
@@ -29,7 +29,7 @@ class Bot:
         self.logger.info("Starting BOT.")
         self.updater = Updater(self.TOKEN)
         self.dispatcher = self.updater.dispatcher
-        self.danger = danger
+        
         
         
         enable_handler = CommandHandler("enable", self.send_enable)
@@ -43,19 +43,10 @@ class Bot:
 
         help_handler = CommandHandler("help", self.send_help)
         self.dispatcher.add_handler(help_handler)
-
-        enable_handler = CommandHandler("live_video", self.live_video)
-        self.dispatcher.add_handler(enable_handler)
          
-        enable_handler = CommandHandler("report", self.report)
+        enable_handler = CommandHandler("reports", self.report)
         self.dispatcher.add_handler(enable_handler)
 
-
-        # force_handler = CommandHandler("force", self.force)
-        # self.dispatcher.add_handler(force_handler)
-
-        # daily_handler = CommandHandler("daily", self.send_daily)
-        # self.dispatcher.add_handler(daily_handler)
 
     # message to send when the bot is started
     def send_start(self, chatbot, update) -> None:
@@ -117,89 +108,30 @@ class Bot:
         chat_id = chatbot.message.chat_id
         db = self._read_db()
         db.set_index('ids',inplace = True)
-
+        string = "Here is the full report:\n"
         if chat_id in db.index:
             if not os.path.exists("bot/reports.txt"):
                 chatbot.message.reply_text("There are no events")
             
-
-            enable_message = 'Hey "User", this is the full report'
             reports = np.genfromtxt('bot/reports.txt',dtype='str')
             for report in reports:
                 string += "•" + report + "\n"
-            chatbot.message.reply_text(enable_message)
             chatbot.message.reply_text(string)
 
         else: 
             chatbot.message.reply_text("Access denied negato❌")
 
     # start the bot
-    def send_alarm(self,timestampm,input_type, label, path) -> int:
+    def run(self) -> int:
+        self.logger.info("Polling BOT.")
+        self.updater.start_polling()
 
-        reports = []
-        current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        db = self._read_db()
-        db.set_index('ids',inplace = True)
-
-        for chat_id in CHAT_IDS:
-            if db.at[chat_id,"status"] == True:
-                # message = "⚠️ *Intrusion Alert* ⚠️\n" + "🕚 " + current_time
-                # self._send_text(message,99706017)
-
-                if input_type == 'video':
-                    self._send_video(open(path, 'rb'),chat_id)
-                elif input_type == 'img':
-                    self._send_img(open(path, 'rb'),label,chat_id)
-        
-        reports.append(current_time)
-        arr = np.genfromtxt('bot/reports.txt',dtype='str')
-        reports = np.append(arr,str(current_time))
-        np.savetxt('reports.txt', reports, delimiter=" ", fmt="%s")
-    
-        # self.logger.info("Polling BOT.")
-        # self.updater.start_polling()
         # Run the BOT until you press Ctrl-C or the process receives SIGINT,
         # SIGTERM or SIGABRT. This should be used most of the time, since
         # start_polling() is non-blocking and will stop the BOT gracefully.
-        # self.updater.idle()
-
-
-    def _send_text(self,bot_message,chat_id):
-        send_text = 'https://api.telegram.org/bot' + self.TOKEN + '/sendMessage?chat_id=' + str(chat_id) + '&parse_mode=Markdown&text=' + bot_message
-        response = requests.get(send_text)
-        return response.json()
-
-
-    def _send_video(self,file_opened,bot_chatID):
-        caption = "⚠️ Intrusion Alert ⚠️\n" + "🕚 " + datetime.now().strftime("%H:%M:%S")
-
-        method = "sendVideo"
-        params = {'chat_id': bot_chatID, 'caption': caption}
-        files = {'video': file_opened}
-        url = 'https://api.telegram.org/bot' + self.TOKEN + "/"
-        response = requests.post(url + method, params, files=files)
-        return response.json()
-
-
-    def _send_img(self,file_opened,label,bot_chatID):
-
-        if label == 'Human':
-            emoticon_label = "📷"
-        else:
-            emoticon_label = "🔈"
-
-        timestamp = datetime.now().strftime("%H:%M:%S")
-
-        caption = f"⚠️ *Intrusion Alert* ⚠️\n\n🕚 {timestamp} \n{emoticon_label} {label}"
-        
-        method = "sendPhoto"
-        params = {'chat_id': bot_chatID, 'caption':caption, 'parse_mode':'Markdown'}
-        files = {'photo': file_opened}
-        url = 'https://api.telegram.org/bot' + self.TOKEN + "/"
-        response = requests.post(url + method, params, files=files)
-        return response.json()
-
-
+        self.updater.idle()
+        return 0
+    
     def _read_db(self):      
         df = pd.read_csv("bot/users.csv")
         return df
@@ -208,3 +140,9 @@ class Bot:
     def _update_db(self,df,index,column,value):
         df.at[index, column] = value
         df.to_csv("bot/users.csv")
+
+if __name__ == "__main__":
+    
+    BOT = Bot()
+    BOT.run()
+ 
