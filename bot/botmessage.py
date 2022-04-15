de78b72ccfd398d1e2870ad9f56280dd0ffab270
GianlucaLM-1 <66356627+GianlucaLM-1@users.noreply.github.com>
2022-04-15 09:40:34 +0200

    Fix bot/botmessage.py

--- bot/botmessage.py
+++ bot/botmessage.py
@@ -82,10 +82,10 @@ class Bot:
                 elif input_type == 'img':
                     self._send_img(open(path, 'rb'),label,chat_id)
         
-        reports.append(current_time)
+        
         arr = np.genfromtxt('bot/reports.txt',dtype='str')
         reports = np.append(arr,str(current_time))
-        np.savetxt('reports.txt', reports, delimiter=" ", fmt="%s")
+        np.savetxt('bot/reports.txt', reports, delimiter=" ", fmt="%s")
     
 
 
