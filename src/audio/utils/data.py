39569c154417fcc837813113ce939f5078091d0a
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-04-15 22:04:25 +0200

    Remove test_ds

--- src/audio/utils/data.py
+++ src/audio/utils/data.py
@@ -1,38 +1,19 @@
-import sys
 
-from constants.path import SPLIT_BASE_PATH, AUGUMENTATION_PATH
+from constants.path import SPLIT_BASE_PATH
 from utils.signal_generator import SignalGenerator
 
 
-def get_data(labels, resampling, mfcc_options, only_train):
+def get_data(labels, resampling, mfcc_options):
 
    with open('{}/train_split.txt'.format(SPLIT_BASE_PATH) ,"r") as fp:
       train_files = [line.rstrip() for line in fp.readlines()]
     
    with open('{}/val_split.txt'.format(SPLIT_BASE_PATH) ,"r") as fp:
-      if not only_train:
-         val_files = [line.rstrip() for line in fp.readlines()]
-      else:
-         for line in fp.readlines():
-            train_files.append(line.rstrip())
-
-
-   with open('{}/test_split.txt'.format(SPLIT_BASE_PATH) ,"r") as fp:
-      if not only_train:
-         test_files = [line.rstrip() for line in fp.readlines()]
-      else:
-         for line in fp.readlines():
-            train_files.append(line.rstrip())   
+      val_files = [line.rstrip() for line in fp.readlines()]
 
    generator = SignalGenerator(labels, sampling_rate=44100, resampling_rate=resampling, **mfcc_options)
-
    
    train_ds = generator.make_dataset(train_files, True, None)
+   val_ds = generator.make_dataset(val_files, False, False)
 
-   if not only_train:
-      val_ds = generator.make_dataset(val_files, False, False)
-      test_ds = generator.make_dataset(test_files, False, False)
-      return train_ds, val_ds , test_ds
-
-   else:
-      return train_ds, None, None
+   return train_ds, val_ds
